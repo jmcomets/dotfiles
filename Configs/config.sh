88349a5c8d9ade398405a5edc79b662714890ad5
Jean-Marie Comets <jean.marie.comets@gmail.com>
2012-08-28 15:50:15 +0200

    Edited the configuration script and the README

--- Configs/config.sh
+++ Configs/config.sh
@@ -1,9 +1,25 @@
 #!/bin/bash
+#
+# Setup script for Bash / Vim configurations.
+# 
+# Author: Jean-Marie Comets <jean.marie.comets@gmail.com>
+# Version: 1.1 - 28/08/2012, 15:41
+#
+# Run as follows:
+#   $ chmod 0777 config.sh
+#   $ ./config.sh
+# Or quicker:
+#   $ bash config.sh
+#
+
+
+# Setup Config's folder link
+ln -s ~/Projects/Configs ~/Configs
 
 # Setup Vim file links
-ln -s ~/Configs/Vim/vimrc ~/.vimrc
 ln -s ~/Configs/Vim/ ~/.vim
+ln -s ~/.vim/vimrc ~/.vimrc
 
 # Setup Bash file links
-ln -fs ~/Configs/Bash/bashrc ~/.bashrc
 ln -s ~/Configs/Bash ~/.bash
+ln -fs ~/.bash/bashrc ~/.bashrc
