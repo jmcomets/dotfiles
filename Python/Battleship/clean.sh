e51ce4c911541b466f862f245df6dc029f086730
Jean-Marie Comets <jean.marie.comets@gmail.com>
2012-08-31 16:20:20 +0200

    Python crap removed, vimrc updated

--- Python/Battleship/clean.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-# Just a small script for cleaning up after
-# that ugly Vim leftover from working on Python files.
-# Deletes all of the compiled `pyc` files
-#
-
-rm -f *.pyc
--- Python/Battleship/user.py
+++
@@ -1,9 +0,0 @@
-class User:
-  """User's definition"""
-  def __init__(self, navy):
-    self.name = ""
-    self.lost = False
-    self.navy = navy
-    self.navy.user = self
-  def lose(self):
-    self.lost = True
