d123a5a645c986e67852558dde9fa9275fa678e4
Jean-Marie Comets <jean.marie.comets@gmail.com>
2012-09-07 18:25:34 +0200

    Wrote a nice and flexible generic raii linked-list (deletion wrapper)

---
+++ CPP/raii_list.hpp
@@ -0,0 +1,54 @@
+#ifndef LIST_H_INCLUDED_
+#define LIST_H_INCLUDED_
+
+namespace utilities
+{
+    template<class T>
+    class raii_list
+    {
+        public:
+            typedef T data_t;
+            explicit raii_list(data_t * data):
+                m_next(0), m_data(data)
+            {
+            }
+
+            ~raii_list()
+            {
+                if (has_next())
+                {
+                    delete m_next;
+                }
+                delete m_data;
+            }
+
+            inline void add(data_t * data)
+            {
+                if (m_data == data)
+                {
+                    return;
+                }
+                else
+                {
+                    if (has_next())
+                    {
+                        m_next->add(data);
+                    }
+                    else
+                    {
+                        m_next = new raii_list(data);
+                    }
+                }
+            }
+
+            inline bool has_next() const
+            {
+                return m_next != 0;
+            }
+        private:
+            raii_list * m_next;
+            data_t * m_data;
+    };
+}
+
+#endif
