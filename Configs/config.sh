d898c77482847a90c4510d6998d761001855fb22
Jean-Marie Comets <jmcomets@jmcomets-K52JB.(none)>
2012-09-19 17:40:38 +0200

    Did a bunch of work on aliases.

--- Configs/config.sh
+++ Configs/config.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 #
 # Setup script for Bash / Vim configurations.
-# 
+#
 # Author: Jean-Marie Comets <jean.marie.comets@gmail.com>
 # Version: 1.1 - 28/08/2012, 15:41
 #
@@ -12,16 +12,15 @@
 #   $ bash config.sh
 #
 
-
 # Setup Config's folder link
-ln -s ~/Projects/Configs ~/Configs
+ln -fs ~/Projects/Configs ~/Configs
 
 # Setup Vim file links
-ln -s ~/Configs/Vim/ ~/.vim
-ln -s ~/.vim/vimrc ~/.vimrc
+ln -fs ~/Configs/Vim/ ~/.vim
+ln -fs ~/.vim/vimrc ~/.vimrc
 
 # Setup Bash file links
-ln -s ~/Configs/Bash ~/.bash
-ln -s ~/.bash/bashrc ~/.bashrc
-ln -s ~/.bash/inputrc ~/.inputrc
-ln -s ~/.bash/dircolors ~/.dircolors
+ln -fs ~/Configs/Bash ~/.bash
+ln -fs ~/.bash/bashrc ~/.bashrc
+ln -fs ~/.bash/inputrc ~/.inputrc
+ln -fs ~/.bash/dircolors ~/.dircolors
