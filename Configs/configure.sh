763c51a6d04bc013698d41de705a5abb238901c3
Jean-Marie Comets <jean.marie.comets@gmail.com>
2013-09-30 18:53:41 +0200

    Fixed shell profile setup

--- Configs/configure.sh
+++ Configs/configure.sh
@@ -3,7 +3,7 @@
 #   Setup script for workspace configuration.
 #
 #   Author: Jean-Marie Comets <jean.marie.comets@gmail.com>
-#   Version: 1.4 - 25/07/2013, 15:26
+#   Version: 1.5 - 30/10/2013, 17:06
 #
 
 WORKSPACE=$HOME
@@ -29,15 +29,19 @@ setup_link $CONFIGS/Bin ~/bin
 setup_link $CONFIGS/Vim ~/.vim
 setup_link ~/.vim/vimrc ~/.vimrc
 
+# Setup shell profile link
+setup_link $CONFIGS/Shell/profile ~/.profile
+
 # Setup Zsh file links
 setup_link $CONFIGS/Shell/zsh ~/.zsh
 setup_link ~/.zsh/rc ~/.zshrc
+setup_link ~/.zsh/env ~/.zshenv
 
 # Setup Bash file links
 setup_link $CONFIGS/Shell/bash ~/.bash
 setup_link ~/.bash/rc ~/.bashrc
-setup_link ~/.bash/profile ~/.profile
 setup_link ~/.bash/inputrc ~/.inputrc
 setup_link ~/.bash/dircolors ~/.dircolors
+setup_link ~/.bash/profile ~/.bash_profile
 
 # vim: ft=sh et sw=4 sts=4
