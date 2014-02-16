556ef1f08303e1acfd5749bae2d48f7eb48b23ae
Jean-Marie Comets <jean.marie.comets@gmail.com>
2014-02-16 12:56:05 +0000

    Arch install, fix cross-compat

--- Dotfiles/configure.sh
+++ Dotfiles/configure.sh
@@ -32,11 +32,11 @@ setup_link ~/.vim/vimrc ~/.vimrc
 setup_link $DOTFILES/Shell/profile ~/.profile
 setup_link $DOTFILES/Shell/dircolors ~/.dircolors
 # and solarized configuration for gnome terminal
-gconftool-2 --set "/apps/gnome-terminal/profiles/Default/use_theme_background" --type bool false
-gconftool-2 --set "/apps/gnome-terminal/profiles/Default/use_theme_colors" --type bool false
-gconftool-2 --set "/apps/gnome-terminal/profiles/Default/palette" --type string "#070736364242:#D3D301010202:#858599990000:#B5B589890000:#26268B8BD2D2:#D3D336368282:#2A2AA1A19898:#EEEEE8E8D5D5:#00002B2B3636:#CBCB4B4B1616:#58586E6E7575:#65657B7B8383:#838394949696:#6C6C7171C4C4:#9393A1A1A1A1:#FDFDF6F6E3E3"
-gconftool-2 --set "/apps/gnome-terminal/profiles/Default/background_color" --type string "#00002B2B3636"
-gconftool-2 --set "/apps/gnome-terminal/profiles/Default/foreground_color" --type string "#65657B7B8383"
+#gconftool-2 --set "/apps/gnome-terminal/profiles/Default/use_theme_background" --type bool false
+#gconftool-2 --set "/apps/gnome-terminal/profiles/Default/use_theme_colors" --type bool false
+#gconftool-2 --set "/apps/gnome-terminal/profiles/Default/palette" --type string "#070736364242:#D3D301010202:#858599990000:#B5B589890000:#26268B8BD2D2:#D3D336368282:#2A2AA1A19898:#EEEEE8E8D5D5:#00002B2B3636:#CBCB4B4B1616:#58586E6E7575:#65657B7B8383:#838394949696:#6C6C7171C4C4:#9393A1A1A1A1:#FDFDF6F6E3E3"
+#gconftool-2 --set "/apps/gnome-terminal/profiles/Default/background_color" --type string "#00002B2B3636"
+#gconftool-2 --set "/apps/gnome-terminal/profiles/Default/foreground_color" --type string "#65657B7B8383"
 
 # Setup Zsh file links
 setup_link $DOTFILES/Shell/zsh ~/.zsh
@@ -48,5 +48,15 @@ setup_link $DOTFILES/Shell/bash ~/.bash
 setup_link ~/.bash/rc ~/.bashrc
 setup_link ~/.bash/inputrc ~/.inputrc
 setup_link ~/.bash/profile ~/.bash_profile
+
+# Git aliases
+git config --global alias.br 'branch'
+git config --global alias.ci 'commit'
+git config --global alias.co 'checkout'
+git config --global alias.st 'status'
+git config --global alias.ciam 'commit --amend'
+git config --global alias.ciamne 'commit --amend --no-edit'
+git config --global user.name 'Jean-Marie Comets'
+git config --global user.email 'jean.marie.comets@gmail.com'
 
 # vim: ft=sh et sw=4 sts=4
